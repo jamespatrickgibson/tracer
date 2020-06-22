06d6ac9eba12615f0e47baa36d95e8f5ada775f6
James Patrick Gibson <james@jamespatrickgibson.com>
2020-06-22 18:37:19 -0400

    Initial Commit

---
+++ Tracer/TracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracerApp.swift
+//  Tracer
+//
+//  Created by James Patrick Gibson on 6/22/20.
+//
+
+import SwiftUI
+
+@main
+struct TracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
